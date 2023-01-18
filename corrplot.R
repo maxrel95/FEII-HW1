e87e98c62b22c56a44dd3f3b37b3da12c1159b6b
maxrel95 <58341766+maxrel95@users.noreply.github.com>
2023-01-18 22:19:52 +0100

    reduce image quality and fancy corr plot R

--- corrplot.R
+++ corrplot.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(data.table) 
 library( corrplot )
 
-corr = as.data.table(fread( "/Users/maxime/Documents/Université/HEC/PhD/6.1/FE I/HW4/corrmat.csv" ))
+corr = as.data.table(fread( "results/corrmat.csv" ))
 corr[,Date:=NULL]
 corr2 = cor( corr )
 
